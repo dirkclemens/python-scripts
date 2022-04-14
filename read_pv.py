3f40b63bbf1c63ad766f234732ef6b80cd306f9a
dirkc <dirkclemens@users.noreply.github.com>
2022-04-14 16:19:22 +0200

    converted to python3

--- read_pv.py
+++ read_pv.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 ########################################################################
 #
 # sudo pip install lxml
-# or  
+# or
 # python3 -m pip install lxml --user
 #
 ########################################################################
@@ -13,7 +13,7 @@
 #import os
 #import time
 #from datetime import datetime
-import urllib
+import urllib.request, urllib.parse, urllib.error
 
 ########################################################################
 #
@@ -24,7 +24,7 @@ import urllib
 # TOTAL			data[1]	*
 # DAILY			data[2]	*
 # STRING1V		data[3] -
-# L1V			data[4] 
+# L1V			data[4]
 # STRING1A		data[5] -
 # L1W			data[6]
 # STRING2V		data[7] *
@@ -36,44 +36,44 @@ import urllib
 # ['2879', '7746', '10.97', '0', '226', '0.00', '962', '484', '228', '6.25', '958', '228', '959']
 ########################################################################
 def readPV():
-	import urllib2
+	import urllib.request, urllib.error, urllib.parse
 	from lxml import html 	# sudo apt-get install python-lxml
 
 	host = 'http://192.168.2.42'
-	password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+	password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
 	password_mgr.add_password(None, host, '***', '***')
-	handler = urllib2.HTTPBasicAuthHandler(password_mgr)
-	opener = urllib2.build_opener(handler)
+	handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+	opener = urllib.request.build_opener(handler)
 	opener.open(host)
-	urllib2.install_opener(opener)
-	response = urllib2.urlopen(host)
-	print ("response: ", response)
+	urllib.request.install_opener(opener)
+	response = urllib.request.urlopen(host)
+	#print(("response: ", response))
 	root = html.fromstring(response.read().strip())
 	data = [v.text.strip() for v in root.xpath("//td[@bgcolor='#FFFFFF']")]
-	print("data: ", data)
+	print(("data: ", data))
 	if "x x x" in data[0]: # check if data is available
 		data[0] = 0
 		data[7] = 0
 		data[9] = 0
 	else:	# send only data, when available
-		print("current: ", data[0] )
-		print("voltage: ", data[7] )
-		print("power: ", data[9] )
-	print("total: ", data[1] )
-	print("daily: ", data[2] )
-	return data	
+		print(("current: ", data[0] ))
+		print(("voltage: ", data[7] ))
+		print(("power: ", data[9] ))
+	print(("total: ", data[1] ))
+	print(("daily: ", data[2] ))
+	return data
 
 
 ########################################################################
-# 
+#
 ########################################################################
 def main():
 	# read PIKO42
 	try:
-		pv = readPV()	
+		pv = readPV()
 		pass
 	except Exception as e:
-		print("Fehler: ", e)
+		print(("Fehler: ", e))
 
 ########################################################################
 #
